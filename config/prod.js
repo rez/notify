9549eb8c4d253a44c36ca9c48d8467bc43427f69
J - <hotcross104@yahoo.com>
2019-01-09 14:49:01 -0500

    add seat key

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,7 @@ module.exports = {
     spotify_client_secret : process.env.SPOTIFY_CLIENT_SECRET,
     mongoDB :  process.env.MONGO_DB,
     cookieKey :  process.env.COOKIE_KEY,
+    seatgeek_client_id : process.env.SEATGEEK_CLIENT_ID,
     spotify_scopes : ['user-read-email', 'user-read-private', 'user-library-read','user-top-read',
         'user-read-recently-played','app-remote-control','streaming', 'user-follow-modify',
         'user-follow-read', 'app-remote-control', 'streaming','user-read-playback-state',
